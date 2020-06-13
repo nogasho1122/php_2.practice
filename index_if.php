29e779b4fdae1cfce437a506435417bf0c84b504
nogasho1122 <exile1122sho@icloud.com>
2020-06-13 10:25:30 +0900

    if文②

--- index_if.php
+++ index_if.php
@@ -1,12 +1,33 @@
 <?php 
 
-$height = '90';
+$height = 90;
 
 var_dump($height);
 
-if ($height === 90){
+if ($height === 91){
   echo '身長は' . $height .'cmです';
+}else{
+  echo '身長は' . $height .'cmではありません';
 }
 
+$signal = 'blue';
+
+if ($signal === 'red'){
+  echo '止まれ';
+}else if ($signal == 'yellow'){
+  echo '一時停止';
+}else{
+  echo '進む';
+}
+
+echo '<br>';
+
+$speed = 80;
+
+if ($signal === 'blue'){
+  if ($speed >= 80){
+    echo 'スピード違反です';
+  }
+}
 
 ?>

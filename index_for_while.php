112304e56c840bc23f96a375cc62c31cfda64a56
nogasho1122 <exile1122sho@icloud.com>
2020-06-13 17:26:49 +0900

    for,while

---
+++ index_for_while.php
@@ -0,0 +1,26 @@
+<?php 
+
+// for 繰り返す数が決まっていたら
+// while　繰り返す数が決まっていなかったら
+
+// break
+// coontinue
+
+for ($i =0; $i < 10; $i++){
+  if ($i === 5) {
+  // break;
+  // continue;
+  
+  }
+  echo $i;
+}
+echo '<br>';
+
+$j =0;
+while($j < 5){
+  echo $j;
+  $j++;
+}
+
+
+?>

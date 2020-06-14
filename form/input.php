a066c7975c9d78caad1e8000eb2d4b9a6deff46d
nogasho1122 <exile1122sho@icloud.com>
2020-06-14 15:47:38 +0900

    フォームセキュリティ　CSRF

--- form/input.php
+++ form/input.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 
 header('X-FRAME-OPTIONS:DENY');
 
@@ -8,14 +9,15 @@ function h($str){
 }
 
 
-// echo '<pre>';
-// var_dump($_POST);
-// echo '</pre>';
+echo '<pre>';
+var_dump($_SESSION);
+echo '</pre>';
 
 // スーパーグローバル変数 php 9種類
 // 連想配列
 
 // 入力、確認、完了　input.php confirm.php thanks.php
+// CSRF 偽物のinput.php
 // input.php　のみ
 
 $pageFlag = 0;
@@ -36,6 +38,16 @@ if (!empty($_POST['btn_submit'])){
 <body>
 
 <?php if ($pageFlag === 0): ?>
+<?php
+if (!isset($_SESSION['csrfToken'])) {
+  $csrfToken = bin2hex(random_bytes(32));
+  $_SESSION['csrfToken'] = $csrfToken;
+}
+
+$token = $_SESSION['csrfToken'];
+
+
+?>
 
 <form method="POST" action= "input.php">
 名前
@@ -45,12 +57,14 @@ if (!empty($_POST['btn_submit'])){
 <input type = "email" name="email" value="<?php echo h($_POST['email']); ?>">
 
 <input type ="submit" name= "btn_confirm" value="確認する">
+<input type ="hidden" name="csrf" value="<?php echo $token; ?>">
 
 </form>
 
 <?php endif; ?>
 
 <?php if ($pageFlag === 1): ?>
+<?php if($_POST['csrf'] === $_SESSION['csrfToken']): ?>
 
 <form method="POST" action= "input.php">
 名前
@@ -63,13 +77,19 @@ if (!empty($_POST['btn_submit'])){
 <input type ="submit" name= "btn_submit" value="送信する">
 <input type ="hidden" name = "your_name" value="<?php echo h($_POST['your_name']); ?>">
 <input type ="hidden" name = "email" value="<?php echo h($_POST['email']); ?>">
+<input type ="hidden" name = "csrf" value="<?php echo h($_POST['csrf']); ?>">
 
 </form>
 
 <?php endif; ?>
+<?php endif; ?>
 
 <?php if ($pageFlag === 2): ?>
+<?php if($_POST['csrf'] === $_SESSION['csrfToken']): ?>
 送信が完了しました。
+
+<?php unset($_SESSION['csrfToken']); ?>
+<?php endif; ?>
 <?php endif; ?>
 
 

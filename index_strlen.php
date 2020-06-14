c9fc1f9ba9c2bfec82e740576b94997ce494af0c
nogasho1122 <exile1122sho@icloud.com>
2020-06-14 10:38:00 +0900

    explode,preg_match 正規表現

--- index_strlen.php
+++ index_strlen.php
@@ -18,4 +18,35 @@ $str = '文字列を置換します';
 
 echo str_replace('置換','ちかん',$str);
 
+echo '<br>';
+
+// 指定文字列で分割
+
+$str_2 = '指定文字列で、分割します';
+
+var_dump(explode('、', $str_2));
+
+// implode 逆
+
+echo '<br>';
+
+// 正規表現
+// 文字がどうか
+// 数字がどうか
+// 郵便番号
+// メールアドレスか test@gmail.com
+
+$str_3 = '特定の文字列が含まれるか確認する';
+
+echo preg_match('/文字列/',$str_3);
+
+echo '<br>';
+
+// 指定文字列から文字列を取得する
+
+echo substr('あいう',2);
+
+echo '<br>';
+echo mb_substr('かきくけこ',2);
+
 ?>

5d77dbc7f2b2bb093f5f2b3f2a64553e24a42e95
nogasho1122 <exile1122sho@icloud.com>
2020-06-14 10:44:01 +0900

    array_push

--- index_strlen.php
+++ index_strlen.php
@@ -49,4 +49,16 @@ echo substr('あいう',2);
 echo '<br>';
 echo mb_substr('かきくけこ',2);
 
+
+// 配列に配列を追加する
+
+$array = ['りんご', 'みかん'];
+
+array_push($array, 'ぶどう','なし');
+
+echo '<pre>';
+var_dump($array);
+echo '</pre>';
+
+
 ?>

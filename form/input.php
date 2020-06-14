bbdf728ea9d2f1a3e0eb648f65eacaa73e3e18b6
nogasho1122 <exile1122sho@icloud.com>
2020-06-14 15:17:28 +0900

    フォーム　入力　確認　完了

--- form/input.php
+++ form/input.php
@@ -1,9 +1,24 @@
 <?php
 
-var_dump($_GET);
 
+echo '<pre>';
+var_dump($_POST);
+echo '</pre>';
 // スーパーグローバル変数 php 9種類
 // 連想配列
+
+// 入力、確認、完了　input.php confirm.php thanks.php
+// input.php　のみ
+
+$pageFlag = 0;
+
+if (!empty($_POST['btn_confirm'])){
+  $pageFlag = 1;
+}
+
+if (!empty($_POST['btn_submit'])){
+  $pageFlag = 2;
+}
 
 ?>
 
@@ -12,20 +27,42 @@ var_dump($_GET);
 <head></head>
 <body>
 
+<?php if ($pageFlag === 0): ?>
+
 <form method="POST" action= "input.php">
 名前
-<input type= "text" name="name"></input>
+<input type= "text" name="your_name" value="<?php echo $_POST['your_name']; ?>"></input>
 <br>
-<input type = "checkbox" name="sports[]" value="野球">野球
-<input type = "checkbox" name="sports[]" value="サッカー">サッカー
-<input type = "checkbox" name="sports[]" value="バスケ">バスケ
+メールアドレス
+<input type = "email" name="email" value="<?php echo $_POST['email']; ?>">
 
-<input type ="submit" value="送信">
-
-
+<input type ="submit" name= "btn_confirm" value="確認する">
 
 </form>
 
+<?php endif; ?>
+
+<?php if ($pageFlag === 1): ?>
+
+<form method="POST" action= "input.php">
+名前
+<?php echo $_POST['your_name']; ?>
+<br>
+メールアドレス
+<?php echo $_POST['email']; ?>
+
+<input type ="submit" name="back" value="戻る">
+<input type ="submit" name= "btn_submit" value="送信する">
+<input type ="hidden" name = "your_name" value="<?php echo $_POST['your_name']; ?>">
+<input type ="hidden" name = "email" value="<?php echo $_POST['email']; ?>">
+
+</form>
+
+<?php endif; ?>
+
+<?php if ($pageFlag === 2): ?>
+送信が完了しました。
+<?php endif; ?>
 
 
 </body>

fa014571e8efac4e46bff405841619e8aadde3be
nogasho1122 <exile1122sho@icloud.com>
2020-06-16 16:33:25 +0900

    ファイル②

--- mainte/test.php
+++ mainte/test.php
@@ -2,31 +2,12 @@
 
 $contactFile = '.contact.dat';
 
+$contents = fopen($contactFile, 'a+');
 
-// ファイルを丸ごと読み込む
-$fileContents = file_get_contents($contactFile);
+$addText = '１行追記' . "\n";
 
-// echo $fileContents;
+fwrite($contents , $addText);
 
-
-// ファイルに書き込み（上書き）
-// file_put_contents($contactFile, 'テストです');
-
-// $addText = 'テストです' . "\n";
-
-// // ファイルに追記
-// file_put_contents($contactFile, $addText, FILE_APPEND);
-
-
-// 配列 file,区切る explode, foreach
-$allData = file($contactFile);
-
-foreach($allData as $lineData){
-  $lines = explode(',' , $lineData);
-  echo $lines[0].'<br>';
-  echo $lines[1].'<br>';
-  echo $lines[2].'<br>';
-}
-
+fclose($contents);
 
 ?>

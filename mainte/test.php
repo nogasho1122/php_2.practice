4ff355a452b719ac9b4ce77b184935a1d8278adf
nogasho1122 <exile1122sho@icloud.com>
2020-06-16 16:25:14 +0900

    ファイル操作

--- mainte/test.php
+++ mainte/test.php
@@ -1,12 +1,32 @@
 <?php
 
-// パスワードを記録した場所
-echo __FILE__;
-// /Applications/MAMP/htdocs/php_2/mainte/test.php
+$contactFile = '.contact.dat';
 
-echo '<br>';
-// パスワード（暗号化）
-(password_hash('password123', PASSWORD_BCRYPT));
+
+// ファイルを丸ごと読み込む
+$fileContents = file_get_contents($contactFile);
+
+// echo $fileContents;
+
+
+// ファイルに書き込み（上書き）
+// file_put_contents($contactFile, 'テストです');
+
+// $addText = 'テストです' . "\n";
+
+// // ファイルに追記
+// file_put_contents($contactFile, $addText, FILE_APPEND);
+
+
+// 配列 file,区切る explode, foreach
+$allData = file($contactFile);
+
+foreach($allData as $lineData){
+  $lines = explode(',' , $lineData);
+  echo $lines[0].'<br>';
+  echo $lines[1].'<br>';
+  echo $lines[2].'<br>';
+}
 
 
 ?>
